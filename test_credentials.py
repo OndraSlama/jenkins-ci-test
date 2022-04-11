7170abe9f51216e8abeb2379d12173b0db7a511a
Ondrej Slama <ondrej.slama@rossum.ai>
2022-04-11 16:10:21 +0200

    Added test_credentials script

---
+++ test_credentials.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    """Parse user name and pass arguments and test agaits test credentials."""
+    parser = ArgumentParser(description='Test credentials.')
+    parser.add_argument('username', help='Username.')
+    parser.add_argument('password', help='Password.')
+    args = parser.parse_args()
+
+    if args.username == 'admin' and args.password == 'admin':
+        print('Access granted.')
+    else:
+        raise Exception('Access denied.')

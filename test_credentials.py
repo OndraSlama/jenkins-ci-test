22b7a49925d79ace388a503e628935dc58f1af80
Ondrej Slama <ondrej.slama@rossum.ai>
2022-04-13 12:41:48 +0200

    test2

--- test_credentials.py
+++ test_credentials.py
@@ -10,5 +10,4 @@ if __name__ == '__main__':
     if args.username == 'admin' and args.password == 'admin':
         print('Access granted.')
     else:
-        print('Access denied.')
         raise Exception('Access denied.')
